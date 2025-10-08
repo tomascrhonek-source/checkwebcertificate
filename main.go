82033d77ed9686b21439e92288dd04edc1982080
Tomas Crhonek <tomascrhonek@gmail.com>
2025-10-08 17:36:51 +0200

    Exit Codes

--- main.go
+++ main.go
@@ -6,12 +6,12 @@ import (
 	"errors"
 	"flag"
 	"log"
+	"os"
 	"time"
 )
 
 func main() {
 	flag.Parse()
-
 	addr := flag.Args()[0]
 
 	// Connect to a TLS server
@@ -19,26 +19,34 @@ func main() {
 	conn, err := tls.Dial("tcp", addr+":443", nil)
 	if errors.As(err, &x509.UnknownAuthorityError{}) {
 		log.Println("Unknown Authority Error:", err)
+		os.Exit(1)
 	} else if errors.Is(err, tls.RecordHeaderError{}) {
 		log.Println("Record Header Error:", err)
+		os.Exit(1)
 	} else if errors.As(err, &x509.HostnameError{}) {
 		log.Println("Hostname Error:", err)
+		os.Exit(1)
 	} else if err != nil {
 		log.Println("Other error:", err)
 	} else if conn.ConnectionState().PeerCertificates == nil {
 		log.Println("No certificates presented by the server")
+		os.Exit(1)
 	}
 	if err != nil {
 		log.Fatalln("Connection error:", err)
+		os.Exit(1)
 	}
+
 	log.Println("Connected to", conn.RemoteAddr())
 	defer conn.Close()
+
 	// Verify that the certificate is for the expected host name
 	log.Println("Verifying hostname:", addr)
 	err = conn.VerifyHostname(addr)
 	if err != nil {
 		// Certificate is for different host name
 		log.Println("Hostname verification error:", err)
+		os.Exit(1)
 	}
 	// Get the certificate chain
 	certs := conn.ConnectionState().PeerCertificates
@@ -50,10 +58,9 @@ func main() {
 		days := time.Until(certDate)
 		log.Printf("Certificate for %s expires on: %s\n", addr, certDate.Format("2006-01-02 15:04:05"))
 		log.Printf("Which is after %.0f days\n", days.Hours()/24)
+		os.Exit(0)
 	} else {
 		log.Println("No certificates found.")
+		os.Exit(1)
 	}
-
 }
-
-// t.In(tz).Format("2006-01-02T15:04:05
