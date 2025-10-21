6a8c01ff1867e85709e1d7dcf20fbb2bd11b096e
Tomas Crhonek <tomascrhonek@gmail.com>
2025-10-21 11:36:43 +0200

    Added debug option

--- main.go
+++ main.go
@@ -24,7 +24,7 @@ func (writer logWriter) Write(bytes []byte) (int, error) {
 	return fmt.Print(time.Now().UTC().Format("2006-01-02 15:04:05 ") + string(bytes))
 }
 
-func checkCerts(addr string) int {
+func checkCerts(addr string, debug *bool) int {
 	var completeAddr string
 
 	if !strings.Contains(addr, ":") {
@@ -36,7 +36,9 @@ func checkCerts(addr string) int {
 	addr = strings.Split(completeAddr, ":")[0]
 
 	// Connect to a TLS server
-	log.Println("Connecting to:", completeAddr)
+	if *debug {
+		log.Println("Connecting to:", completeAddr)
+	}
 	conn, err := tls.Dial("tcp", completeAddr, nil)
 	if errors.As(err, &x509.UnknownAuthorityError{}) {
 		log.Println("Unknown Authority Error:", err)
@@ -59,18 +61,26 @@ func checkCerts(addr string) int {
 		os.Exit(1)
 	}
 
-	log.Println("Connected to", conn.RemoteAddr())
+	if *debug {
+		log.Println("Connected to", conn.RemoteAddr())
+	}
 	defer conn.Close()
 
 	// Verify that the certificate is for the expected host name
-	log.Println("Verifying hostname:", addr)
+	if *debug {
+		log.Println("Verifying hostname:", addr)
+	}
 	err = conn.VerifyHostname(addr)
 	if err != nil {
 		// Certificate is for different host name
 		log.Println("Hostname verification error:", err)
 		os.Exit(1)
 	}
+
 	// Get the certificate chain
+	if *debug {
+		log.Println("Getting the certificate chain")
+	}
 	certs := conn.ConnectionState().PeerCertificates
 
 	// Extract and print the expiry date of the first certificate
@@ -82,8 +92,7 @@ func checkCerts(addr string) int {
 		log.Printf("Which is after %.0f days\n", days.Hours()/24)
 		return int(days.Hours() / 24)
 	} else {
-		log.Println("No certificates found.")
-		os.Exit(1)
+		log.Fatal("No certificates found.")
 	}
 	return -1
 }
@@ -100,25 +109,45 @@ func main() {
 	log.SetOutput(new(logWriter))
 
 	prom := flag.Bool("prometheus", false, "Export data for Prometheus")
+	debug := flag.Bool("debug", false, "Debugging informations")
+	port := flag.Int("port", 2112, "Port for prometheus exporter, default 2112")
+
 	flag.Parse()
-	addr := flag.Args()[0]
+
+	if *debug {
+		log.Println("Prometheus:", *prom)
+		log.Println("Port:", *port)
+	}
 
 	if len(flag.Args()) != 1 {
 		fmt.Println("Usage: checkwebcertificate domain")
 		os.Exit(1)
 	}
 
+	addr := flag.Args()[0]
+
+	if *debug {
+		log.Println("Domain:", addr)
+	}
+
 	if *prom {
 		go func() {
 			for {
-				crtDays.Set(float64(checkCerts(addr)))
+				crtDays.Set(float64(checkCerts(addr, debug)))
 				time.Sleep(10 * time.Second)
 			}
 		}()
 
 		http.Handle("/metrics", promhttp.Handler())
-		http.ListenAndServe(":2112", nil)
+		p := fmt.Sprintf(":%d", *port)
+		if *debug {
+			log.Println("Listening on:", p)
+		}
+		err := http.ListenAndServe(p, nil)
+		if err != nil {
+			log.Fatal(err)
+		}
 	} else {
-		checkCerts(addr)
+		checkCerts(addr, debug)
 	}
 }
